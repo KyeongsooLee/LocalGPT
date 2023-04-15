3b91027e823713d626f1e249c07e5b79a3ce5c49
KyeongsooLee <leeksca26@gmail.com>
2023-04-15 18:58:27 -0400

    first commit!

---
+++ node-chatgpt-api.js
@@ -0,0 +1,54 @@
+const { Configuration, OpenAIApi } = require("openai");
+const readlineSync = require("readline-sync");
+const fs = require("fs");
+require("dotenv").config();
+
+(async () => {
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  const historyFile = "history.json";
+  let history = [];
+
+  if (fs.existsSync(historyFile)) {
+    const historyData = fs.readFileSync(historyFile);
+    history = JSON.parse(historyData);
+  }
+
+  while (true) {
+    const user_input = readlineSync.question("Your input: ");
+
+    const messages = [];
+    for (const [input_text, completion_text] of history) {
+      messages.push({ role: "user", content: input_text });
+      messages.push({ role: "assistant", content: completion_text });
+    }
+
+    messages.push({ role: "user", content: user_input });
+
+    try {
+      const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: messages,
+      });
+
+      const completion_text = completion.data.choices[0].message.content;
+      console.log(completion_text);
+
+      history.push([user_input, completion_text]);
+
+      const historyData = JSON.stringify(history, null, 2);
+      fs.writeFileSync(historyFile, historyData);
+
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.status);
+        console.log(error.response.data);
+      } else {
+        console.log(error.message);
+      }
+    }
+  }
+})();
